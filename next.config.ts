69ab19c56eca14e6e1494d936809ea6b559e5f95
karolchmori <72026498+karolchmori@users.noreply.github.com>
2026-06-03 01:59:34 +0200

    MOD - Second try deploy website

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,11 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: { unoptimized: true },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  output: "export", // Keep this for GitHub Pages
-  images: {
-    unoptimized: true, // Required for GitHub Pages
-  },
 };
 
-const withMDX = createMDX({
-  // This helps Next.js recognize MDX files during the build
-  extension: /\.mdx?$/,
-});
+const withMDX = createMDX({});
 
 export default withMDX(nextConfig);
