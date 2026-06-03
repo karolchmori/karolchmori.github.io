c444f5ebfe5e72957ed0aa231e77b467c38b7a8b
KarolChale <karolchale@hotmail.com>
2026-06-03 02:57:28 +0200

    NEW - 1th Test Astro Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://karolchmori.github.io',
+    base: '/',
     integrations: [mdx(), sitemap(), react()],
     fonts: [
         {
