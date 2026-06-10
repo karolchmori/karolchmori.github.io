0c2c6de814fb8b46c05b3f6346b0bf2c3d2b94c7
KarolChale <karolchale@hotmail.com>
2026-06-11 00:13:55 +0200

    MOD - VideoModal autoplay

--- src/lib/video.ts
+++ src/lib/video.ts
@@ -1,11 +1,11 @@
 export type VideoPlatform = "vimeo" | "youtube";
 
-export function getVideoUrl(platform: VideoPlatform, id: string) {
+export function getVideoUrl(platform: VideoPlatform, id: string, autoplay: number) {
   switch (platform) {
     case "vimeo":
-      return `https://player.vimeo.com/video/${id}`;
+      return `https://player.vimeo.com/video/${id}?autoplay=${autoplay}`;
     case "youtube":
-      return `https://www.youtube.com/embed/${id}`;
+      return `https://www.youtube.com/embed/${id}?autoplay=${autoplay}`;
     default:
       throw new Error(`Unsupported platform: ${platform}`);
   }
