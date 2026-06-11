9bee5e61fbd185fb7713594660dca6bf659e9864
KarolChale <karolchale@hotmail.com>
2026-06-11 23:37:54 +0200

    ADD - SoftwareTool object and library

---
+++ src/data/ToolLibrary.ts
@@ -0,0 +1,66 @@
+import type { SoftwareTool } from '../objects/SoftwareTool';
+
+
+const PAGES_ROOTPATH = '../pages/tools';
+const ICON_ROOTPATH = '/software';
+
+const rawTools = [
+  {
+    id: "Houdini",
+    label: "Houdini",
+    enabled: true,
+  },
+  {
+    id: "Maya",
+    label: "Maya",
+    enabled: true,
+  },
+  {
+    id: "Nuke",
+    label: "Nuke",
+    enabled: true,
+  },
+  {
+    id: "UnrealEngine",
+    label: "Unreal Engine",
+    enabled: false,
+  },
+  {
+    id: "Notion",
+    label: "Notion",
+    enabled: true,
+  },
+  {
+    id: "SubstancePainter",
+    label: "Substance Painter",
+    enabled: true,
+  },
+  {
+    id: "Photoshop",
+    label: "Adobe Photoshop",
+    enabled: false,
+  },
+  {
+    id: "Windows",
+    label: "Windows",
+    enabled: true,
+  },
+  {
+    id: "Blender",
+    label: "Blender",
+    enabled: false,
+  },
+];
+
+
+
+export const toolLibrary: SoftwareTool[] = rawTools.map(tool => ({
+  ...tool,
+  iconPath: `${ICON_ROOTPATH}/AS_${tool.id}.svg`,
+  pagesPath: `${PAGES_ROOTPATH}/${tool.id}`,
+}));
+
+
+export function getActiveTools(): SoftwareTool[] {
+  return toolLibrary.filter(tool => tool.enabled);
+}
---
+++ src/objects/SoftwareTool.ts
@@ -0,0 +1,8 @@
+export interface SoftwareTool {
+    id: string;
+    label: string;
+    iconPath: string; 
+    pagesPath: string;
+    enabled: boolean;
+
+}
